5e7b65f51141696f1ec9ad5030532445424eaf7a
lsefiane <lyes.sefiane@gmail.com>
2022-01-01 12:14:53 -0500

    ~ Docker Deployment Scripts
    - Docker Compose
    - SHELL Deployment
    - Flyway Schema Init
    - Environment variables

---
+++ deployment/flyway/sql_versions/V1__init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS network_device
+(ipaddress varchar(255) NOT NULL,
+ element_type varchar(255) NOT NULL,
+ CONSTRAINT network_device_PK PRIMARY KEY(ipaddress));
+
+CREATE TABLE IF NOT EXISTS neighbor
+(ipaddress varchar(255) NOT NULL,
+CONSTRAINT neighbor_pk PRIMARY KEY (ipaddress));
+
+CREATE TABLE IF NOT EXISTS connection (
+    network_device_ipaddress varchar(255) NOT NULL REFERENCES network_device(ipaddress) ON UPDATE CASCADE ON DELETE CASCADE,
+    neighbor_ipaddress varchar(255) NOT NULL REFERENCES neighbor(ipaddress) ON UPDATE CASCADE ON DELETE CASCADE,
+    cost INTEGER NOT NULL,
+    UNIQUE(network_device_ipaddress, neighbor_ipaddress),
+    CONSTRAINT connection_pk PRIMARY KEY(network_device_ipaddress, neighbor_ipaddress));
