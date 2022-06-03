e03033f81a522bc5e4a3f7603e75b290c3f7183b
lsefiane <lyes.sefiane@gmail.com>
2022-06-03 13:28:08 -0400

    ~ JaCoCo Module Aggregated Report in CircleCi

---
+++ jacoco-coverage-report-service/src/main/java/com/lyess/jacococoveragereportservice/JacocoCoverageReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lyess.jacococoveragereportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoCoverageReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoCoverageReportServiceApplication.class, args);
+	}
+
+}
